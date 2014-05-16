f9eff2db360eec4339115f8d9cee7ffe5d0aaace
Jason Goldstein <jason@betheshoe.com>
2014-05-16 13:10:59 -0400

    v1.0.12

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import curation
 
 setup(
     name='django-curation',
-    version="1.0.11",
+    version="1.0.12",
     description='A model used for curating other models and proxying their attributes',
     author='The Atlantic',
     author_email='atmoprogrammers@theatlantic.com',
