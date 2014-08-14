66ebb7df86a676fd48e338e8158961e72d45ddd0
Frankie Dintino <fdintino@theatlantic.com>
2014-08-14 18:31:31 -0400

    Bump version to v1.0.15

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import curation
 
 setup(
     name='django-curation',
-    version="1.0.14",
+    version="1.0.15",
     description='A model used for curating other models and proxying their attributes',
     author='The Atlantic',
     author_email='atmoprogrammers@theatlantic.com',
