3daf66c12b2e352e7d14fc7ea47cbd8d8adcb0c9
Frankie Dintino <fdintino@theatlantic.com>
2016-01-29 14:47:21 -0500

    v1.0.34

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import curation
 
 setup(
     name='django-curation',
-    version="1.0.33",
+    version="1.0.34",
     description='A model used for curating other models and proxying their attributes',
     author='The Atlantic',
     author_email='atmoprogrammers@theatlantic.com',
