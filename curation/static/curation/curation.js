theatlantic/django-curation

commit c2601e3d515100bd9c2591a76887e0c235fae80d

Fix double initialization when ContentTypeSourceField has default kwarg

curation/static/curation/curation.js
@@ -348,7 +348,7 @@
 
     $(document).ready(function() {
         // Iterate through curated content_type select elements
-        $('.curated-content-type-select').each(function(index, element) {
+        $('.curated-content-type-select:not([id*="__prefix__"]').each(function(index, element) {
             $(element).curated_content_type();
         });
     });

commit f317fbe7cbb585778eb43e9455c57315f288a879

Fix Safari bug introduced in c2601e3

curation/static/curation/curation.js
@@ -348,8 +348,12 @@
 
     $(document).ready(function() {
         // Iterate through curated content_type select elements
-        $('.curated-content-type-select:not([id*="__prefix__"]').each(function(index, element) {
-            $(element).curated_content_type();
+        $('.curated-content-type-select').each(function() {
+            // Skip Django 'empty' forms
+            if (this.id.indexOf("__prefix__") > -1) {
+                return;
+            }
+            $(this).curated_content_type();
         });
     });
 